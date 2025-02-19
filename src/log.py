d6e8a0edbb2f081ae6b65fdfc2201bfdbe7424f8
MarcoParola <marcoparola96@gmail.com>
2025-02-19 18:34:22 +0100

    implement wandb logger

---
+++ src/log.py
@@ -0,0 +1,18 @@
+import flatdict
+from omegaconf import DictConfig, OmegaConf
+
+def hp_from_cfg(cfg):
+    cfg = OmegaConf.to_container(cfg, resolve=True)
+    return dict(flatdict.FlatDict(cfg, delimiter="/"))
+
+def get_loggers(cfg):
+    """Returns a list of loggers
+    cfg: hydra config
+    """
+    from pytorch_lightning.loggers import WandbLogger
+    import wandb
+    hyperparameters = hp_from_cfg(cfg)
+    wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project)
+    wandb.config.update(hyperparameters)
+    wandb_logger = WandbLogger()
+    return wandb_logger
